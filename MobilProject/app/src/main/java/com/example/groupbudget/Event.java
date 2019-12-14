831f92a74be53fc7cf54db6d20b740734c4730dd
Jens Van Goethem <jensvangoethem2000@gmail.com>
2019-12-14 23:45:12 +0100

    Update Event en Group scherm

--- MobilProject/app/src/main/java/com/example/groupbudget/Event.java
+++ MobilProject/app/src/main/java/com/example/groupbudget/Event.java
@@ -18,15 +18,56 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
 public class Event extends AppCompatActivity {
+
     private Button submitEventBtn;
+    private EditText et2;
+    private String st2;
+    private EditText et3;
+    private String st3;
+    private EditText et4;
+    private String st4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        submitEventBtn = (Button) findViewById(R.id.submitEventBtn);
+        et2 = findViewById(R.id.editText2);
+        et3 = findViewById(R.id.editText3);
+        et4 = findViewById(R.id.editText4);
+        submitEventBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Invullen1();
+
+                Intent i3 = new Intent(Event.this, Group.class);
+                st3 = et3.getText().toString();
+                i3.putExtra("", st3);
+                startActivity(i3);
+
+                finish();
+
+                Intent i4 = new Intent(Event.this, Group.class);
+                st4 = et4.getText().toString();
+                i4.putExtra("", st4);
+                startActivity(i4);
+
+                finish();
+            }
+        });
+    }
+
+    public void Invullen1(){
+
+        Intent i2 = new Intent(Event.this, Group.class);
+        st2 = et2.getText().toString();
+        i2.putExtra("Momenteel is er nog geen activiteit vast gesteld", st2);
+        startActivity(i2);
+        finish();
     }
 }
--- MobilProject/app/src/main/java/com/example/groupbudget/Group.java
+++ MobilProject/app/src/main/java/com/example/groupbudget/Group.java
@@ -28,15 +28,29 @@ public class Group extends AppCompatActivity {
     private Button addEventBtn;
     private TextView tv;
     private String st;
+    private TextView tv2;
+    private String st2;
+    private TextView tv3;
+    private String st3;
+    private TextView tv4;
+    private String st4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
         tv = findViewById(R.id.textView);
+        tv2 = findViewById(R.id.textView2);
+        tv3 = findViewById(R.id.textView3);
+        tv4 = findViewById(R.id.textView4);
         st = getIntent().getExtras().getString("Value");
+        st2 = getIntent().getExtras().getString("Momenteel is er nog geen activiteit vast gesteld");
+        st3 = getIntent().getExtras().getString("");
+        st4 = getIntent().getExtras().getString("");
         tv.setText(st);
-
+        tv2.setText(st2);
+        tv3.setText(st3);
+        tv4.setText(st4);
         addEventBtn = (Button) findViewById(R.id.addEventBtn);
         addEventBtn.setOnClickListener(new View.OnClickListener() {
             @Override
