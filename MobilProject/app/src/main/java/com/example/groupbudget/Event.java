2f7926f34dced34d90e384d19ee7b448a58e883d
Jens Van Goethem <jensvangoethem2000@gmail.com>
2019-12-14 15:17:46 +0100

    Event met Group link

--- MobilProject/app/src/main/java/com/example/groupbudget/Event.java
+++ MobilProject/app/src/main/java/com/example/groupbudget/Event.java
@@ -1,14 +1,43 @@
 package com.example.groupbudget;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
 public class Event extends AppCompatActivity {
+    private Button submitEventBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        submitEventBtn = (Button) findViewById(R.id.addEventBtn);
+        submitEventBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openAddGroupScreen();
+            }
+        });
+    }
+    public void openAddGroupScreen(){
+        Intent intent = new Intent(this,Group.class);
+        startActivity(intent);
     }
 }
