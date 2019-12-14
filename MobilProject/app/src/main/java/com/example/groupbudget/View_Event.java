73a0a4a4705cc6437cf56edc8ce5875010c73113
Wannes <wanvinken@gmail.com>
2019-12-14 15:15:36 +0100

    Samenvoeging projecten
    View_group en View_event activities toegevoegd en Main aangepast

---
+++ MobilProject/app/src/main/java/com/example/groupbudget/View_Event.java
@@ -0,0 +1,14 @@
+package com.example.groupbudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__event);
+    }
+}
---
+++ MobilProject/app/src/main/java/com/example/groupbudget/View_Group.java
@@ -0,0 +1,14 @@
+package com.example.groupbudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Group extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__group);
+    }
+}
